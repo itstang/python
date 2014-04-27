3b722288103aad2449c78424fa0651a85bad104e
Richard Tang <richrdtang@gmail.com>
2014-04-27 13:11:23 -0500

    Getting hitbox detection started

--- pygame/main.py
+++ pygame/main.py
@@ -8,10 +8,22 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
+PADDLE_HEIGHT = 75
+PADDLE_WIDTH = 20
+
+CENTER_SCREEN = 225
+
 pygame.init()
 
+class Paddle(object):
+	def __init__(self, xpos, ypos):
+		self.x = xpos
+		self.y = ypos + 200
+		self.hitHeight = ypos + PADDLE_HEIGHT
+		self.hitWidth = self.x + PADDLE_WIDTH
+
 def drawPaddle(screen, x, y):
-	pygame.draw.rect(screen, WHITE, [x,225+y,20,75])
+	pygame.draw.rect(screen, WHITE, [x,CENTER_SCREEN+y,PADDLE_WIDTH,PADDLE_HEIGHT])
 
 size = (700, 500)
 screen = pygame.display.set_mode(size)
@@ -23,11 +35,11 @@ clock = pygame.time.Clock()
 y_coord = 0
 y_speed = 0
 
-rect_x = 50
-rect_y = 50
+ball_x = 50
+ball_y = 50
 
-rect_x_vel = 5
-rect_y_vel = 3
+ball_x_vel = 5
+ball_y_vel = 3
 # Main Game Loop
 while not done:
 	# Main event loop
@@ -40,6 +52,10 @@ while not done:
 				y_speed = -5
 			if event.key == pygame.K_DOWN:
 				y_speed = 5
+			if event.key == pygame.K_ESCAPE:
+				done = True
+			if event.key == pygame.K_RETURN:
+				print(player1.hitWidth)
 
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_UP:
@@ -52,27 +68,39 @@ while not done:
 	# Drawing Code
 	screen.fill(BLACK)
 
-	pygame.draw.ellipse(screen, WHITE, [rect_x,rect_y,25,25])
+	pygame.draw.ellipse(screen, WHITE, [ball_x,ball_y,25,25])
 
-	if rect_y > 475 or rect_y < 0:
-		rect_y_vel *= -1
-	if rect_x > 675 or rect_x < 0:
-		rect_x_vel *= -1
+	# Border Checking -- Will need to change
+	if ball_y > 475 or ball_y < 0:
+		ball_y_vel *= -1
+	if ball_x > 675 or ball_x < 0:
+		ball_x_vel *= -1
 
-	rect_x += rect_x_vel
-	rect_y += rect_y_vel
+	# Animate the ball
+	ball_x += ball_x_vel
+	ball_y += ball_y_vel
 
+	# Animate the paddle
 	y_coord += y_speed
 
 	if y_coord > 200:
 		y_coord = 200
-		print("hit")
-	if y_coord < -200:
-		y_coord == 0
-		print("top")
 
-	drawPaddle(screen, 0, y_coord)
-	drawPaddle(screen, 680, 0)
+	if y_coord < -225:
+		y_coord = -225	
+
+	player1 = Paddle(0, 0)
+	player2 = Paddle(680, 0)
+
+	drawPaddle(screen, player1.x, y_coord)
+	drawPaddle(screen, player2.x, 0)
+
+	player1.y = y_coord
+	print(player1.hitHeight)
+	#print(ball_y)
+
+	if ball_x == player1.hitWidth and ball_y > player1.y:
+		ball_x_vel *= -1
 
 	pygame.display.flip()
 	clock.tick(60)
